35a953be9bb73f9822f4c2cd191d5ffdcb089325
Hasibul Islam Nirjhar <hasibulislam@iut-dhaka.edu>
2023-08-17 12:52:33 +0600

    Lab 1 Task 1

--- Lab 1/Lab1.py
+++ Lab 1/Lab1.py
@@ -1,4 +1,27 @@
 
 
+def CalculateAverageGpa(gradesfile):
+
+    content =open(gradesfile,"r")
+    lines=content.readlines()
+    content.close()
+
+    totalGpa=0
+
+    for line in lines:
+        totalGpa+=float(line.split(";")[1])
+
+    AverageGpa=(round(totalGpa/len(lines), 3))
+
+    print(AverageGpa)
+
+CalculateAverageGpa("grades.txt")
+
+
+
+
+
+
+
 
 
